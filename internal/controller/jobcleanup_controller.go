c21bf5fe54862ad1556805977e341f0bcfdcf98c
aakanksha-arun <desai.aaka@northeastern.edu>
2025-03-21 20:15:13 -0400

    feat: add finalizer for cleanup

--- internal/controller/jobcleanup_controller.go
+++ internal/controller/jobcleanup_controller.go
@@ -5,7 +5,9 @@ import (
 	"time"
 
 	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,10 +39,53 @@ func (r *JobCleanupReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	if job.Status.Succeeded > 0 || job.Status.Failed > 0 {
 		log.Info("Cleaning up completed/failed job", "job", job.Name)
 
+		log.Info("Job is queued for deletion", "job", job.Name)
+		if len(job.GetFinalizers()) > 0 {
+			log.Info("Finalizer present, performing cleanup", "job", job.Name)
+
+			// List all pods in the same namespace as the job
+			podList := &corev1.PodList{}
+			labelSelector := metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"job-name": job.Name,
+				},
+			}
+			selector, err := metav1.LabelSelectorAsSelector(&labelSelector)
+			if err != nil {
+				log.Error(err, "Failed to convert label selector")
+				return ctrl.Result{}, err
+			}
+			opts := client.ListOptions{LabelSelector: selector, Namespace: job.Namespace}
+			if err := r.List(ctx, podList, &opts); err != nil {
+				log.Error(err, "Failed to list pods")
+				return ctrl.Result{}, err
+			}
+			for _, pod := range podList.Items {
+				log.Info("Deleting pod", "pod", pod.Name)
+				if err := r.Delete(ctx, &pod); err != nil {
+					log.Error(err, "Failed to delete pod", "pod", pod.Name)
+					return ctrl.Result{}, err
+				}
+			}
+			job.SetFinalizers(nil)
+			if err := r.Update(ctx, job); err != nil {
+				log.Error(err, "Failed to remove finalizer")
+				return ctrl.Result{}, err
+			}
+
+		}
 		if err := r.Delete(ctx, job); err != nil {
+			if errors.IsNotFound(err) {
+				log.Info("Job not found. It might have already been deleted.")
+				return ctrl.Result{}, nil
+			}
 			log.Error(err, "Failed to delete job")
 			return ctrl.Result{}, err
 		}
+
+		log.Info("Job deleted successfully")
+		return ctrl.Result{}, nil
+
 	}
 	// Requeue periodically (e.g., every 1 minute)
 	return ctrl.Result{RequeueAfter: 1 * time.Minute}, nil
